e217a44a9f11b2fe5a7c7fe29b68bffd529543f5
c-h-a-r-l-i-e <c-h-a-r-l-i-e@users.noreply.github.com>
2020-03-25 12:33:49 +0000

    Evaluate motion filtering

--- data/evaluation/testmotionfilter.py
+++ data/evaluation/testmotionfilter.py
@@ -12,12 +12,95 @@ import filtering
 import data
 import motionfilter
 import peakfind
+import csv
 
 
-
-def test_validity(ax1, ax2, step, nlms=True, filterx=True, filtery=False, filterz=False, taps=20):
+def write_validity_table(filename, nlms=True):
     segmentsize = 10
     ppgs, ecgs, accxs, accys, acczs = testsyncs.getSegmentsAtNoise(testsyncs.NOISE_MEDIUM, segmentsize)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        def booltxt(d):
+            if d:
+                return 'Y'
+            else:
+                return 'N'
+
+        orders = {}
+        for x in [True, False]:
+            for y in [True, False]:
+                for z in [True, False]:
+                    orders["{}{}{}".format(booltxt(x), booltxt(y), booltxt(z))] = 0
+
+        k = 0 
+
+        for taps in [1,2,3,4,5,6,7,8]:
+            for step in [0.01,0.1,0.5,1,2]:
+                abs_errors = {}
+                for x in [True, False]:
+                    for y in [True, False]:
+                        for z in [True, False]:
+                            average_error = 0
+                            for i in range(len(ppgs)):
+                                # Remove unrelated noise
+                                ppg = filtering.butter_bandpass_filter(ppgs[i], 0.4, 4, order=2)
+                                accx = filtering.butter_bandpass_filter(accxs[i], 0.4, 4, order=2)
+                                accy = filtering.butter_bandpass_filter(accys[i], 0.4, 4, order=2)
+                                accz = filtering.butter_bandpass_filter(acczs[i], 0.4, 4, order=2)
+
+                                # Apply motion filter to each axis dependant on filter parameters
+                                filtered = ppg
+                                if x:
+                                    filtered = motionfilter.adaptiveFilter(filtered, accx, step, nlms, M=taps)
+                                if y:
+                                    filtered = motionfilter.adaptiveFilter(filtered, accy, step, nlms, M=taps)
+                                if z:
+                                    filtered = motionfilter.adaptiveFilter(filtered, accz, step, nlms, M=taps)
+                                
+                                # Calculate heart-rate from filtered signal, and compare to ECG heart-rate
+                                filteredpeaks = peakfind.find_peaks_min_sd(filtered)
+                                filteredrate = filteredpeaks.size / segmentsize * 60
+
+                                ecgvals = ecgs[i].getValues()
+                                filtered_ecg = hp.remove_baseline_wander(ecgvals, ecgs[i].getFrequency())
+                                wd, m = hp.process(hp.scale_data(filtered_ecg), ecgs[i].getFrequency())
+                                ecgrate = m["bpm"]
+
+                                average_error += np.abs(ecgrate - filteredrate) / len(ppgs)
+
+
+                            abs_errors["{}{}{}".format(booltxt(x), booltxt(y), booltxt(z))] = average_error
+
+                            writer.writerow([taps, step, booltxt(x), booltxt(y), booltxt(z), round(average_error, 3)])
+
+                            print("step={},nlms={},x={},y={},z={},taps={} --> average error = {}".format(step, 
+                                nlms, x, y, z, taps, average_error))
+
+                
+                print("This iterations errors {}".format(abs_errors))
+                abs_errors.pop("NNN")
+
+                j = 1
+                for error in sorted(abs_errors.items(), key=lambda x: x[1]):
+                    orders[error[0]] += j
+                    j+=1
+
+                print("Total orders {}".format(orders))
+                print("k : {}".format(k))
+                k += 1
+
+        print("Total orders {}".format(orders))
+        print("k : {}".format(k))
+
+
+                        
+
+
+
+def test_validity(ax1, ax2, step, nlms=True, filterx=True, filtery=False, filterz=False, taps=20, noise=testsyncs.NOISE_MEDIUM):
+    segmentsize = 10
+    ppgs, ecgs, accxs, accys, acczs = testsyncs.getSegmentsAtNoise(noise, segmentsize)
 
     average_error = 0
     errors = np.zeros(len(ppgs))
@@ -59,11 +142,11 @@ def test_validity(ax1, ax2, step, nlms=True, filterx=True, filtery=False, filter
         nlms, filterx, filtery, filterz, taps, average_error))
 
     #plt.figure(num="step={},nlms={},taps={}".format(step, nlms, taps))
-    ax1.plot(xs, ys, label="Number of taps {}".format(taps))
+    ax1.plot(xs, ys, label="{} taps".format(taps))
 
     xs = np.sort(np.abs(errors))
     
-    ax2.plot(xs, ys, label="Number of taps {}".format(taps))
+    ax2.plot(xs, ys, label="{} taps".format(taps))
 
 
         
@@ -109,17 +192,19 @@ def test_validity_butter(order):
     print("average product : {}".format(average_product))
 
 
-def plot_adaptive_validity():
+
+
+
+def plot_adaptive_validity(nlms, noise):
     ax1 = plt.subplot(121)
     ax2 = plt.subplot(122)
-    nlms = False 
-    for taps in [1,3,4,5,6,8]:
+    for taps in [1,5,10,15,20]:
         for step in [1]: 
             print("########   taps = {}, steps = {}  ############".format(taps, step))
             for x in [True]:
                 for y in [False]:
                     for z in [False]:
-                        test_validity(ax1, ax2, step, nlms, x, y, z, taps=taps)
+                        test_validity(ax1, ax2, step, nlms, x, y, z, taps=taps, noise=noise)
 
     ax1.legend()
     ax1.set_title("Error")
@@ -134,13 +219,22 @@ def plot_adaptive_validity():
     ax2.set_ylabel("Cumulative probability")
     ax2.set_ylim(0,1)
     ax2.set_xlim(0,200)
-    plt.show()
 
 if __name__ == "__main__":
-    plot_adaptive_validity()
+    #write_validity_table("lms_validity.csv", nlms=False)
+    plt.figure("nlms, medium")
+    plot_adaptive_validity(True, testsyncs.NOISE_MEDIUM)
 
+    plt.figure("nlms, high")
+    plot_adaptive_validity(True, testsyncs.NOISE_HIGH)
 
+    plt.figure("lms, medium")
+    plot_adaptive_validity(False, testsyncs.NOISE_MEDIUM)
 
+    plt.figure("lms, high")
+    plot_adaptive_validity(False, testsyncs.NOISE_HIGH)
+
+    plt.show()
 
 
 #    for i in (0.1,0.2,0.5,1,2,5):
